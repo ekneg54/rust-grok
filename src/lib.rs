179b2a42bfca404151f7f557a2d9870465b381fc
ekneg54 <ekneg54@pm.me>
2023-02-25 16:46:33 +0000

    add add pattern

--- src/lib.rs
+++ src/lib.rs
@@ -6,31 +6,53 @@ use pyo3::prelude::*;
 #[pyclass]
 pub struct Grok {
     pub obj: grok::Grok,
-    pub pattern: grok::Pattern,
+    pub pattern: Option<grok::Pattern>,
+}
+
+impl Default for Grok {
+    fn default() -> Self {
+        Self {
+            obj: grok::Grok::with_default_patterns(),
+            pattern: None,
+        }
+    }
 }
 
 #[pymethods]
 impl Grok {
     #[new]
-    pub fn __new__(pattern_str: &str) -> PyResult<Self> {
-        let mut obj: grok::Grok = grok::Grok::with_default_patterns();
-        let pattern: grok::Pattern = obj
-            .compile(pattern_str, false)
-            .expect("Error while compiling");
+    pub fn __new__() -> Self {
+        return Grok::default();
+    }
 
-        Ok(Self { obj, pattern })
+    pub fn compile(&mut self, pattern_str: &str) {
+        self.pattern = Some(
+            self.obj
+                .compile(pattern_str, false)
+                .expect("Error while compiling"),
+        );
     }
 
     pub fn match_against(&self, target: &str) -> HashMap<String, String> {
         let mut res: HashMap<String, String> = HashMap::new();
-        let matches: Option<grok::Matches> = self.pattern.match_against(target);
-        if matches.is_some() {
-            for (k, v) in matches.unwrap().iter() {
-                res.insert(k.to_string(), v.to_string());
+        if self.pattern.is_some() {
+            let matches: Option<grok::Matches> =
+                self.pattern.as_ref().unwrap().match_against(target);
+            if matches.is_some() {
+                for (k, v) in matches.unwrap().iter() {
+                    res.insert(k.to_string(), v.to_string());
+                }
+                return res;
+            } else {
+                return res;
             }
+        } else {
             return res;
         }
-        return res;
+    }
+
+    pub fn add_pattern(&mut self, name: &str, pattern: &str) {
+        self.obj.add_pattern(name, pattern);
     }
 }
 
--- tests/test_grok.py
+++ tests/test_grok.py
@@ -1,16 +1,27 @@
 # pylint: disable=no-member
 # pylint: disable=missing-docstring
+import pytest
 import rustgrok
+
 
 class TestGrok:
 
-    def test_init(self):
-        grok = rustgrok.Grok("%{USERNAME}")
+    def test_init_without_pattern(self):
+        rustgrok.Grok()
+
+    def test_init_with_pattern_raises(self):
+        with pytest.raises(TypeError):
+            rustgrok.Grok("pattern")
+
+    def test_basic_usecase(self):
+        grok = rustgrok.Grok()
+        grok.compile("%{USERNAME}")
         result = grok.match_against("root")
         assert result.get("USERNAME") == "root"
 
     def test_multi_match(self):
-        grok = rustgrok.Grok("%{IP:client} %{WORD:method} %{URIPATHPARAM:request} %{NUMBER:bytes} %{NUMBER:duration}")
+        grok = rustgrok.Grok()
+        grok.compile("%{IP:client} %{WORD:method} %{URIPATHPARAM:request} %{NUMBER:bytes} %{NUMBER:duration}")
         result = grok.match_against("55.3.244.1 GET /index.html 15824 0.043")
         assert result.get("client") == "55.3.244.1"
         assert result.get("method") == "GET"
@@ -19,6 +30,14 @@ class TestGrok:
         assert result.get("duration") == "0.043"
 
     def test_no_match_returns_empty_dict(self):
-        grok = rustgrok.Grok("%{IP:client} %{WORD:method}")
+        grok = rustgrok.Grok()
+        grok.compile("%{IP:client} %{WORD:method}")
         result = grok.match_against("bla bla")
         assert result == {}
+
+    def test_add_custom_pattern(self):
+        grok = rustgrok.Grok()
+        grok.add_pattern("USER", "root")
+        grok.compile("%{USER}")
+        result = grok.match_against("root")
+        assert result == {"USER": "root"}
